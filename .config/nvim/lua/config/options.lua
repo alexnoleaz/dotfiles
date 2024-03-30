216dd419c7d1f9f19c8697c82742a3ebdef662bd
Alexander Nole <cnole_azabache@outlook.com>
2024-03-30 17:16:37 -0500

    Update options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,26 +1,35 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
 vim.g.editorconfig = true
+vim.g.mapleader = " "
+
 local opt = vim.opt
+
+opt.encoding = "utf-8"
+opt.fileencoding = "utf-8"
+
+opt.title = true
+opt.autoindent = true
+opt.hlsearch = true
+opt.backup = false
+opt.showcmd = true
+opt.cmdheight = 1
+opt.laststatus = 2
+opt.scrolloff = 10
+opt.backupskip = { "/tmp/*", "/private/tmp/" }
+opt.inccommand = "split"
+opt.smarttab = true
+opt.breakindent = true
+opt.backspace = { "start", "eol", "indent" }
+opt.path:append({ "**" })
+opt.wildignore:append({ "*/node_modules/*" })
+opt.splitkeep = "cursor"
+-- opt.mouse = ""
 opt.wrap = true
 opt.swapfile = false
+opt.formatoptions:append({ "r" })
 
--- if vim.loop.os_uname().sysname == "Windows_NT" then
---   if vim.o.shell == '"C:\\Program Files\\Git\\usr\\bin\\bash.exe"' then
---     opt.shell = "C:\\Program Files\\Git\\bin\\bash.exe"
---     opt.shellcmdflag = "-s"
---   else
---     opt.shell = "pwsh" or "powershell"
---     opt.shellcmdflag =
---       "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
---     opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
---     opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
---     opt.shellquote = ""
---     opt.shellxquote = ""
---   end
--- end
+-- Undercurl
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
 
 if vim.fn.has("win32") == 1 then
   if vim.o.shell == '"C:\\Program Files\\Git\\usr\\bin\\bash.exe"' then
